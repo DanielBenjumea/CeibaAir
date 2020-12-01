4695f8f0538953dcf102f80502efffa292c14528
Daniel Mejia Benjumea - Ceiba Software <daniel.benjumea@ceiba.com.co>
2020-12-01 09:22:28 -0500

    borrados imports sin utilizar

--- src/infraestructura/usuario/controlador/usuario.controlador.ts
+++ src/infraestructura/usuario/controlador/usuario.controlador.ts
@@ -1,17 +1,14 @@
-import { Body, Controller, Get, Post, Request, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ComandoRegistrarUsuario } from 'src/aplicacion/usuario/comando/registrar-usuario.comando';
 import { ManejadorRegistrarUsuario } from 'src/aplicacion/usuario/comando/registar-usuario.manejador';
 import { ManejadorListarUsuario } from 'src/aplicacion/usuario/consulta/listar-usuarios.manejador';
 import { UsuarioDto } from 'src/aplicacion/usuario/consulta/dto/usuario.dto';
-import { ManejadorSignInUsuario } from 'src/aplicacion/usuario/comando/signin-usuario.manejador';
-import { AuthGuard } from '@nestjs/passport';
 
 @Controller('usuarios')
 export class UsuarioControlador {
 	constructor(
 		private readonly _manejadorRegistrarUsuario: ManejadorRegistrarUsuario,
-		private readonly _manejadorListarUsuario: ManejadorListarUsuario,
-		private readonly _manejadorSignInUsuario: ManejadorSignInUsuario
+		private readonly _manejadorListarUsuario: ManejadorListarUsuario
 	) {}
 
 	@Post()
