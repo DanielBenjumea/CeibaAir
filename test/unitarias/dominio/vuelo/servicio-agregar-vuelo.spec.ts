2ac622ba6378138c3baec41c36835f9f7435b775
Daniel Mejia Benjumea - Ceiba Software <daniel.benjumea@ceiba.com.co>
2020-12-05 21:17:21 -0500

    pruebas unitarias guardar vuelo

--- test/unitarias/dominio/vuelo/servicio-agregar-vuelo.spec.ts
+++ test/unitarias/dominio/vuelo/servicio-agregar-vuelo.spec.ts
@@ -1,8 +1,24 @@
+import { SinonStubbedInstance } from 'sinon';
+import { Usuario } from 'src/dominio/usuario/modelo/usuario';
+import { Vuelo } from 'src/dominio/vuelo/modelo/Vuelo';
+import { RepositorioVuelo } from 'src/dominio/vuelo/puerto/repositorio/repositorio-vuelo';
 import { ServicioAgregarVuelo } from 'src/dominio/vuelo/servicio/servicio-agregar-vuelo';
+import { createStubObj } from 'test/util/create-object.stub';
 
 describe('ServicioAgregarVuelo', () => {
-	let ServicioAgregarVuelo: ServicioAgregarVuelo;
-	it('TODO', async () => {
-		expect(true).toBe(true);
+	let servicioAgregarVuelo: ServicioAgregarVuelo;
+	let repositorioVueloStub: SinonStubbedInstance<RepositorioVuelo>;
+
+	beforeEach(() => {
+		repositorioVueloStub = createStubObj<RepositorioVuelo>([ 'guardar' ]);
+		servicioAgregarVuelo = new ServicioAgregarVuelo(repositorioVueloStub);
+	});
+	it('debería guardar el vuelo en el repositorio', async () => {
+		const vuelo = new Vuelo('Bogotá', 'Cali', 500000, new Date().toISOString(), new Array<Usuario>());
+
+		await servicioAgregarVuelo.ejecutar(vuelo);
+
+		expect(repositorioVueloStub.guardar.getCalls().length).toBe(1);
+		expect(repositorioVueloStub.guardar.calledWith(vuelo)).toBeTruthy();
 	});
 });
