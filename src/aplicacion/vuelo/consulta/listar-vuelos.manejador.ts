84c9f7424e88aa549c768de5ec04894167529038
Daniel Mejia Benjumea - Ceiba Software <daniel.benjumea@ceiba.com.co>
2020-12-06 16:22:31 -0500

    fixed code smells

--- src/aplicacion/vuelo/consulta/listar-vuelos.manejador.ts
+++ src/aplicacion/vuelo/consulta/listar-vuelos.manejador.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { DaoVuelo } from 'src/dominio/vuelo/puerto/dao/dao-vuelo';
 import { VueloEntidad } from 'src/infraestructura/vuelo/entidad/vuelo.entidad';
-import { VueloDto } from './dto/vuelo.dto';
 
 @Injectable()
 export class ManejadorListarVuelo {
--- src/dominio/vuelo/puerto/dao/dao-vuelo.ts
+++ src/dominio/vuelo/puerto/dao/dao-vuelo.ts
@@ -1,5 +1,4 @@
 import { VueloEntidad } from 'src/infraestructura/vuelo/entidad/vuelo.entidad';
-import { VueloDto } from '../../../../aplicacion/vuelo/consulta/dto/vuelo.dto';
 
 export abstract class DaoVuelo {
 	abstract async listar(): Promise<VueloEntidad[]>;
